ef769373669e5156913f9657610808290e12dd4c
Alex Malik <xsikor@gmail.com>
2014-09-11 17:19:31 +0300

    Added css, style loader (set width and height by background image size)

--- mlp.js
+++ mlp.js
@@ -1,5 +1,5 @@
 function Mlp() {
-	console.log("MLP START!");
+	console.log("MyLittlePlayer init.");
 	this.count = 0;
 	//Create new player from selector
 	this.Select = function(selector) {
@@ -16,28 +16,27 @@ function Mlp() {
 
 	this.Create = function(selector) {
 		var elem = this.Select(selector);
+		if(elem == false)
+			return elem
 		var tmp = document.createElement('div');
 		tmp.setAttribute("class", "mlp-player");
-		tmp.setAttribute("id", "mlp-"+(this.count++).toString());
+		tmp.setAttribute("id", "mlp-"+(++this.count).toString());
 		tmp.appendChild(elem.cloneNode(true));
 		tmp.innerHTML += this.markup;
 		elem.outerHTML = tmp.outerHTML;
-		return tmp;
-	}
-
-	//test
-	this.AddEvents = function(elem) {
-		var
-			control = elem.getElementsByClassName("play-pause")[0],
+		elem = this.Select("#mlp-"+this.count.toString());
+		//need to remove this shit or meybe not
+		var control = elem.getElementsByClassName("control")[0],
 			progress = elem.getElementsByClassName("progress")[0],
 			loaded = elem.getElementsByClassName("loaded")[0],
 			time = elem.getElementsByClassName("time")[0],
 			volume_ctrl = elem.getElementsByClassName("volume-ctrl")[0],
 			vol_ico = volume_ctrl.getElementsByClassName("icon")[0],
 			vol_scrub = volume_ctrl.getElementsByClassName("scrubber")[0],
-			message = elem.getElementsByClassName("message")[0];
+			message = elem.getElementsByClassName("message")[0],
+			player = elem.getElementsByTagName("audio")[0];
 
-		var children = {
+		var ret = {
 			"control": control,
 			"progress": progress,
 			"loaded": loaded,
@@ -45,16 +44,46 @@ function Mlp() {
 			"volume_ctrl": volume_ctrl,
 			"vol_ico": vol_ico,
 			"vol_scrub": vol_scrub,
-			"message": message
-		}
+			"message": message,
+			"root": elem,
+			"player": player
+		}	
 
-		return children;
+		return ret;
+	}
+
+	//test
+	this.AddEvents = function(obj) {
+		obj.control.addEventListener("click", this.PlayPause);
+	}
+
+	this.LoadCss = function(obj) {
+		for(k in obj) {
+			var el = obj[k];
+			var bg = getStyle(el, "backgroundImage") || false;
+			bg = bg.slice(bg.indexOf("http"), bg.indexOf('")'))
+			if(bg.length > 5) {
+				var img = new Image();
+				img.src = bg;
+				el.style.width = img.width;
+				el.style.height = img.height;
+			}
+		}
+	}
+
+	this.PlayPause = function(e) {
+		var isPlay = (e.target.className.indexOf("pause") != -1) ? false : true;
+		if(isPlay) {
+			e.target.className = "control-pause";
+		} else {
+			e.target.className = "control-play";
+		}
 	}
 
 
 
 	this.markup = '\
-		<div class="play-pause"></div>\
+		<div class="control"></div>\
 		<div class="body">\
 			<div class="progress"></div>\
 			<div class="loaded"></div>\
@@ -65,4 +94,13 @@ function Mlp() {
 			<div class="scrubber"></div>\
 		</div>\
 		<div class="message"></div>';
+}
+
+function getStyle(elem, option) {
+	var option = option || false;
+	ret = window.getComputedStyle(elem);
+	if(option)
+		return ret[option] || false;
+	else
+		return ret;
 }
